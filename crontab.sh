3620768b86d7dc3a6c0f34ed3ef11b48e8661bff
ishan dutta <ishandutta2007@gmail.com>
2020-10-24 14:28:27 +0530

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,3 @@
+0 * * * * bash /Users/ishandutta2007/Documents/Projects/InstaCurator/random_ic_onemin.sh
+40 * * * * bash /Users/ishandutta2007/Documents/Projects/HootsuiteLite/shellscripsts/random_allapis_onemin.sh
+
